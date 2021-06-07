edb4a5d906058ec9cc2a602e079288c94eb13445
kiyaamodien <kiyaamudienkhan@gmail.com>
2021-06-07 16:11:04 +0200

    first commit

---
+++ dom.js
@@ -0,0 +1,27 @@
+// //EXAMINE THE DOCUMENT OBJECT//
+
+// // console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.type);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = "Hello";
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// //GETELEMENTBYID//
+// // console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById("mainheader");
+// var headerTitle = document.getElementById("header-title");
+// // console.log(headerTitle);
+// // headerTitle.textContent = 'Hello';
+// // headerTitle.innerText = 'Goodbye';
+// // console.log(headerTitle.innerText);
+// // headerTitle.innerHTML = <h3>Hello</h3>;
+// headerTitle.style.borderBottom = "solid 3px #000";
